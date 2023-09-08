49738dfec23a20405a358904c66ae5e6719f5188
UGO SOFIA <ugo.sofia97@gmail.com>
2023-09-08 20:09:36 +0200

    modifiche allo script pandoc

--- html2epub.sh
+++ html2epub.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+if [ $# -lt 1 ]
+then
+	echo "No arguments supplied. Please provide the path to a markdown file"
+	exit 1
+fi
+
 input=$1
 output=$2
 
 pandoc $input -o $output \
 --css=ebook.css \
 --epub-cover-image=Fondamenti.png \
+epub_version="3.0"
+toc_level=2
